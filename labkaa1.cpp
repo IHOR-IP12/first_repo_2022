40e7e375ceaa1dab8c9f7f57a3a570ee405fa52a
IHOR-IP12 <119290087+IHOR-IP12@users.noreply.github.com>
2022-11-28 11:29:15 +0200

    Add files via upload

---
+++ labkaa1.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+	double z, y, res;
+	z = 3.127;
+	y = 7.315;
+	const int e = 2.718;
+
+	res = 16 * pow(y, 2) + pow(e, y) * pow(e, z) + pow(z, 1 / 3) + pow(1.51, 1 / 3) + log(y * z);
+
+
+	printf("res = %f\n", res);
+	return 0;
+}
+
+
+
+
+
+
+
+
